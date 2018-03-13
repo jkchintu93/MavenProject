a890b1187acfa7ec4a754eff32269bbcee338477
jkchintu93 <jklikhitkar@gmail.com>
2018-03-13 13:28:14 +0530

    second commit

---
+++ src/qsp/DemoB.java
@@ -0,0 +1,12 @@
+package qsp;
+
+public class DemoB {
+	public static void main(String[] args) {
+		String str="10";
+		int x=10;
+		int y =Integer.parseInt(str);
+		System.out.println(y==x);
+		
+	}
+
+}
